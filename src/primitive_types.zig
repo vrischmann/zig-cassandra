c62bb8e53670db2dddbf88e23d63783cdbbcb8aa
Vincent Rischmann <vincent@rischmann.fr>
2020-03-30 02:29:49 +0200

    primitive: remove CQLVersion since it's actually not always 3.0.0

--- src/primitive_types.zig
+++ src/primitive_types.zig
@@ -3,10 +3,6 @@ const builtin = @import("builtin");
 const mem = std.mem;
 const testing = std.testing;
 
-pub const CQLVersion = enum {
-    V3_0_0,
-};
-
 pub const ProtocolVersion = packed enum(u8) {
     V3,
     V4,
