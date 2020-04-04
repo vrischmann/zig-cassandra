675f5a7d1a860312a27bcd927441e54c93a09bab
Vincent Rischmann <vincent@rischmann.fr>
2020-04-04 19:21:40 +0200

    improve event frame tests

--- src/main.zig
+++ src/main.zig
@@ -1464,7 +1464,7 @@ test "event frame: status change" {
     testing.expect(net.Address.eql(localhost, status_change.node_address));
 }
 
-test "event frame: schema change" {
+test "event frame: schema change/keyspace" {
     const data = "\x84\x00\xff\xff\x0c\x00\x00\x00\x2a\x00\x0d\x53\x43\x48\x45\x4d\x41\x5f\x43\x48\x41\x4e\x47\x45\x00\x07\x43\x52\x45\x41\x54\x45\x44\x00\x08\x4b\x45\x59\x53\x50\x41\x43\x45\x00\x06\x62\x61\x72\x62\x61\x7a";
     var fbs = std.io.fixedBufferStream(data);
     var in_stream = fbs.inStream();
@@ -1487,6 +1487,58 @@ test "event frame: schema change" {
     testing.expectString("barbaz", options.keyspace);
     testing.expectString("", options.object_name);
     testing.expect(options.arguments == null);
+}
+
+test "event frame: schema change/table" {
+    const data = "\x84\x00\xff\xff\x0c\x00\x00\x00\x2e\x00\x0d\x53\x43\x48\x45\x4d\x41\x5f\x43\x48\x41\x4e\x47\x45\x00\x07\x43\x52\x45\x41\x54\x45\x44\x00\x05\x54\x41\x42\x4c\x45\x00\x06\x66\x6f\x6f\x62\x61\x72\x00\x05\x73\x61\x6c\x75\x74";
+    var fbs = std.io.fixedBufferStream(data);
+    var in_stream = fbs.inStream();
+
+    var framer = Framer(@TypeOf(in_stream)).init(testing.allocator, in_stream);
+    _ = try framer.readHeader();
+
+    checkHeader(Opcode.Event, data.len, framer.header);
+
+    const frame = try EventFrame.read(testing.allocator, @TypeOf(framer), &framer);
+    defer frame.deinit();
+
+    testing.expect(frame.event == .SCHEMA_CHANGE);
+
+    const schema_change = frame.event.SCHEMA_CHANGE;
+    testing.expectEqual(SchemaChangeType.CREATED, schema_change.type);
+    testing.expectEqual(SchemaChangeTarget.TABLE, schema_change.target);
+
+    const options = schema_change.options;
+    testing.expectString("foobar", options.keyspace);
+    testing.expectString("salut", options.object_name);
+    testing.expect(options.arguments == null);
+}
+
+test "event frame: schema change/function" {
+    const data = "\x84\x00\xff\xff\x0c\x00\x00\x00\x40\x00\x0d\x53\x43\x48\x45\x4d\x41\x5f\x43\x48\x41\x4e\x47\x45\x00\x07\x43\x52\x45\x41\x54\x45\x44\x00\x08\x46\x55\x4e\x43\x54\x49\x4f\x4e\x00\x06\x66\x6f\x6f\x62\x61\x72\x00\x0d\x73\x6f\x6d\x65\x5f\x66\x75\x6e\x63\x74\x69\x6f\x6e\x00\x01\x00\x03\x69\x6e\x74";
+    var fbs = std.io.fixedBufferStream(data);
+    var in_stream = fbs.inStream();
+
+    var framer = Framer(@TypeOf(in_stream)).init(testing.allocator, in_stream);
+    _ = try framer.readHeader();
+
+    checkHeader(Opcode.Event, data.len, framer.header);
+
+    const frame = try EventFrame.read(testing.allocator, @TypeOf(framer), &framer);
+    defer frame.deinit();
+
+    testing.expect(frame.event == .SCHEMA_CHANGE);
+
+    const schema_change = frame.event.SCHEMA_CHANGE;
+    testing.expectEqual(SchemaChangeType.CREATED, schema_change.type);
+    testing.expectEqual(SchemaChangeTarget.FUNCTION, schema_change.target);
+
+    const options = schema_change.options;
+    testing.expectString("foobar", options.keyspace);
+    testing.expectString("some_function", options.object_name);
+    const arguments = options.arguments.?;
+    testing.expectEqual(@as(usize, 1), arguments.len);
+    testing.expectString("int", arguments[0]);
 }
 
 test "auth challenge frame" {
