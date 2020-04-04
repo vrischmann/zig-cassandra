8f357894ef6ea32b066c340e514e6aed6006a5aa
Vincent Rischmann <vincent@rischmann.fr>
2020-04-04 02:22:02 +0200

    implement RegisterFrame and test it

--- src/main.zig
+++ src/main.zig
@@ -414,6 +414,7 @@ const ExecuteFrame = struct {
     }
 };
 
+/// Structure of a query in a BATCH frame
 const BatchQuery = struct {
     const Self = @This();
 
@@ -558,7 +559,29 @@ const BatchFrame = struct {
 /// REGISTER is sent to register this connection to receive some types of events.
 ///
 /// Described in the protocol spec at §4.1.8
-const RegisterFrame = struct {};
+const RegisterFrame = struct {
+    const Self = @This();
+
+    allocator: *mem.Allocator,
+
+    event_types: [][]const u8,
+
+    pub fn deinit(self: *const Self) void {
+        for (self.event_types) |s| {
+            self.allocator.free(s);
+        }
+        self.allocator.free(self.event_types);
+    }
+
+    pub fn read(allocator: *mem.Allocator, comptime FramerType: type, framer: *FramerType) !RegisterFrame {
+        var frame: Self = undefined;
+        frame.allocator = allocator;
+
+        frame.event_types = (try framer.readStringList()).toOwnedSlice();
+
+        return frame;
+    }
+};
 
 // Below are response frames only (ie server -> client).
 
@@ -1108,4 +1131,23 @@ test "batch frame: query type prepared" {
     defer frame.deinit();
 
     testing.expectEqual(BatchType.Logged, frame.batch_type);
+}
+
+test "register frame" {
+    const data = "\x04\x00\x00\xc0\x0b\x00\x00\x00\x31\x00\x03\x00\x0f\x54\x4f\x50\x4f\x4c\x4f\x47\x59\x5f\x43\x48\x41\x4e\x47\x45\x00\x0d\x53\x54\x41\x54\x55\x53\x5f\x43\x48\x41\x4e\x47\x45\x00\x0d\x53\x43\x48\x45\x4d\x41\x5f\x43\x48\x41\x4e\x47\x45";
+    var fbs = std.io.fixedBufferStream(data);
+    var in_stream = fbs.inStream();
+
+    var framer = Framer(@TypeOf(in_stream)).init(testing.allocator, in_stream);
+    _ = try framer.readHeader();
+
+    checkHeader(Opcode.Register, data.len, framer.header);
+
+    const frame = try RegisterFrame.read(testing.allocator, @TypeOf(framer), &framer);
+    defer frame.deinit();
+
+    testing.expectEqual(@as(usize, 3), frame.event_types.len);
+    testing.expectString("TOPOLOGY_CHANGE", frame.event_types[0]);
+    testing.expectString("STATUS_CHANGE", frame.event_types[1]);
+    testing.expectString("SCHEMA_CHANGE", frame.event_types[2]);
 }
