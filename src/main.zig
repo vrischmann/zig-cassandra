8c926ed82acfdebae2b62c30a719e648d405ca78
Vincent Rischmann <vincent@rischmann.fr>
2020-04-04 19:51:58 +0200

    move RegisterFrame to its own file

--- src/main.zig
+++ src/main.zig
@@ -12,35 +12,6 @@ const testing = @import("testing.zig");
 
 // Below are request frames only (ie client -> server).
 
-/// REGISTER is sent to register this connection to receive some types of events.
-///
-/// Described in the protocol spec at §4.1.8
-const RegisterFrame = struct {
-    const Self = @This();
-
-    allocator: *mem.Allocator,
-
-    event_types: [][]const u8,
-
-    pub fn deinit(self: *const Self) void {
-        for (self.event_types) |s| {
-            self.allocator.free(s);
-        }
-        self.allocator.free(self.event_types);
-    }
-
-    pub fn read(allocator: *mem.Allocator, comptime FramerType: type, framer: *FramerType) !Self {
-        var frame = Self{
-            .allocator = allocator,
-            .event_types = undefined,
-        };
-
-        frame.event_types = (try framer.readStringList()).toOwnedSlice();
-
-        return frame;
-    }
-};
-
 // Below are response frames only (ie server -> client).
 
 // TODO(vincent): test all error codes
@@ -621,25 +592,6 @@ test "supported frame" {
     testing.expectEqual(CompressionAlgorithm.LZ4, frame.compression_algorithms[1]);
 }
 
-test "register frame" {
-    const data = "\x04\x00\x00\xc0\x0b\x00\x00\x00\x31\x00\x03\x00\x0f\x54\x4f\x50\x4f\x4c\x4f\x47\x59\x5f\x43\x48\x41\x4e\x47\x45\x00\x0d\x53\x54\x41\x54\x55\x53\x5f\x43\x48\x41\x4e\x47\x45\x00\x0d\x53\x43\x48\x45\x4d\x41\x5f\x43\x48\x41\x4e\x47\x45";
-    var fbs = std.io.fixedBufferStream(data);
-    var in_stream = fbs.inStream();
-
-    var framer = Framer(@TypeOf(in_stream)).init(testing.allocator, in_stream);
-    _ = try framer.readHeader();
-
-    checkHeader(Opcode.Register, data.len, framer.header);
-
-    const frame = try RegisterFrame.read(testing.allocator, @TypeOf(framer), &framer);
-    defer frame.deinit();
-
-    testing.expectEqual(@as(usize, 3), frame.event_types.len);
-    testing.expectEqualString("TOPOLOGY_CHANGE", frame.event_types[0]);
-    testing.expectEqualString("STATUS_CHANGE", frame.event_types[1]);
-    testing.expectEqualString("SCHEMA_CHANGE", frame.event_types[2]);
-}
-
 test "auth challenge frame" {
     // TODO(vincent): how do I get one of these frame ?
 }
@@ -652,7 +604,7 @@ test "" {
     _ = @import("frames/prepare.zig");
     _ = @import("frames/execute.zig");
     _ = @import("frames/batch.zig");
-    // _ = @import("frames/register.zig");
+    _ = @import("frames/register.zig");
     // _ = @import("frames/error.zig");
     // _ = @import("frames/ready.zig");
     // _ = @import("frames/authenticate.zig");
