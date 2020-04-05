3c03b47b668806e3d2905c3670eb81ec0dfecee3
Vincent Rischmann <vincent@rischmann.fr>
2020-04-05 17:17:02 +0200

    testing: add a function to return a arena allocator

--- src/testing.zig
+++ src/testing.zig
@@ -2,6 +2,7 @@ const std = @import("std");
 
 pub const allocator = std.testing.allocator;
 pub const expect = std.testing.expect;
+pub const expectError = std.testing.expectError;
 pub const expectEqual = std.testing.expectEqual;
 pub const expectEqualSlices = std.testing.expectEqualSlices;
 
@@ -10,4 +11,8 @@ pub fn expectEqualString(a: []const u8, b: []const u8) void {
     if (!std.mem.eql(u8, a, b)) {
         std.debug.panic("expected string \"{}\", got \"{}\"", .{ a, b });
     }
+}
+
+pub fn arenaAllocator() std.heap.ArenaAllocator {
+    return std.heap.ArenaAllocator.init(std.testing.allocator);
 }
