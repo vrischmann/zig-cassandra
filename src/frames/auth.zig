20164249b961192222eea2fa1f121a6e01ea1672
Vincent Rischmann <vincent@rischmann.fr>
2020-04-08 00:34:01 +0200

    frames: merge auth and authenticate

--- src/frames/auth.zig
+++ src/frames/auth.zig
@@ -8,6 +8,19 @@ const Framer = @import("../framer.zig").Framer;
 usingnamespace @import("../primitive_types.zig");
 const testing = @import("../testing.zig");
 
+/// AUTHENTICATE is sent by a node in response to a STARTUP frame if authentication is required.
+///
+/// Described in the protocol spec at §4.2.3.
+const AuthenticateFrame = struct {
+    authenticator: []const u8,
+
+    pub fn read(allocator: *mem.Allocator, comptime FramerType: type, framer: *FramerType) !AuthenticateFrame {
+        return AuthenticateFrame{
+            .authenticator = try framer.readString(),
+        };
+    }
+};
+
 /// AUTH_RESPONSE is sent to a node to answser a authentication challenge.
 ///
 /// Described in the protocol spec at §4.1.2.
@@ -46,6 +59,24 @@ const AuthSuccessFrame = struct {
         };
     }
 };
+
+test "authenticate frame" {
+    var arena = testing.arenaAllocator();
+    defer arena.deinit();
+
+    const data = "\x84\x00\x00\x00\x03\x00\x00\x00\x31\x00\x2f\x6f\x72\x67\x2e\x61\x70\x61\x63\x68\x65\x2e\x63\x61\x73\x73\x61\x6e\x64\x72\x61\x2e\x61\x75\x74\x68\x2e\x50\x61\x73\x73\x77\x6f\x72\x64\x41\x75\x74\x68\x65\x6e\x74\x69\x63\x61\x74\x6f\x72";
+    var fbs = std.io.fixedBufferStream(data);
+    var in_stream = fbs.inStream();
+
+    var framer = Framer(@TypeOf(in_stream)).init(&arena.allocator, in_stream);
+    _ = try framer.readHeader();
+
+    checkHeader(Opcode.Authenticate, data.len, framer.header);
+
+    const frame = try AuthenticateFrame.read(&arena.allocator, @TypeOf(framer), &framer);
+
+    testing.expectEqualString("org.apache.cassandra.auth.PasswordAuthenticator", frame.authenticator);
+}
 
 test "auth challenge frame" {
     // TODO(vincent): how do I get one of these frame ?
