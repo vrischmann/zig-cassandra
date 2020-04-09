5311fd72eb994603691d42b874f356a2b508b2a2
Vincent Rischmann <vincent@rischmann.fr>
2020-04-09 22:25:34 +0200

    more work on the iterator

--- src/iterator.zig
+++ src/iterator.zig
@@ -39,14 +39,10 @@ const Iterator = struct {
             const field_type_info = @typeInfo(struct_field.field_type);
             const field_name = struct_field.name;
 
-            switch (field_type_info) {
-                .Array => |array| {
-                    @field(args, field_name) = try self.readArray(i, struct_field.field_type);
-                },
-                .Int => |int| {},
-                .Pointer => |pointer| {},
-                else => @compileError("field type " ++ @typeName(struct_field.field_type) ++ " not handled yet"),
-            }
+            const column_spec = self.metadata.column_specs[@as(usize, i)];
+            const column_data = self.rows[self.pos].slice[@as(usize, i)];
+
+            @field(args, field_name) = try self.readType(column_spec, column_data, struct_field.field_type);
         }
 
         self.pos += 1;
@@ -54,35 +50,63 @@ const Iterator = struct {
         return true;
     }
 
-    fn readArray(self: *Self, comptime column: comptime_int, comptime Type: type) !Type {
-        const column_data = self.rows[self.pos].slice[@as(usize, column)];
-        const column_spec = self.metadata.column_specs[@as(usize, column)];
+    fn readType(self: *Self, column_spec: ColumnSpec, column_data: ColumnData, comptime Type: type) !Type {
+        const type_info = @typeInfo(Type);
 
+        switch (type_info) {
+            .Array => {
+                return try self.readArray(column_spec, column_data, Type);
+            },
+            .Int => {
+                return try self.readInt(column_spec, column_data, Type);
+            },
+            .Pointer => |pointer| switch (pointer.size) {
+                .One => {
+                    return try self.readType(column_spec, column_data, @TypeOf(pointer.child));
+                },
+                .Slice => {
+                    return try self.readSlice(column_spec, column_data, Type, @TypeOf(pointer.child));
+                },
+                else => @compileError("invalid pointer size " ++ @tagName(pointer.size)),
+            },
+            else => @compileError("field type " ++ @typeName(struct_field.field_type) ++ " not handled yet"),
+        }
+    }
+
+    fn readInt(self: *Self, column_spec: ColumnSpec, column_data: ColumnData, comptime Type: type) !Type {
+        var r: Type = 0;
+
+        return r;
+    }
+
+    fn readSlice(self: *Self, column_spec: ColumnSpec, column_data: ColumnData, comptime Type: type, comptime ChildType: type) !Type {
+        var slice: Type = undefined;
+
+        switch (column_spec.option.id) {
+            .Blob, .UUID, .Timeuuid => {
+                slice = column_data.slice;
+            },
+            .Ascii, .Varchar => {
+                slice = column_data.slice;
+            },
+            else => unreachable,
+        }
+
+        return slice;
+    }
+
+    fn readArray(self: *Self, column_spec: ColumnSpec, column_data: ColumnData, comptime Type: type) !Type {
         var array: Type = undefined;
 
         switch (column_spec.option.id) {
-            // .Ascii, .Varchar => {
-            //     if (std.meta.Elem(Type) != u8) {
-            //         @compileError("unable to read an Ascii string in type " ++ @typeName(Type));
-            //     }
-            //     if (array.len < column_data.slice.len) {
-            //         return error.ArrayTooSmall;
-            //     }
-            //     mem.copy(u8, &array, column_data.slice);
-            // },
             .UUID, .Timeuuid => {
-                if (array.len < column_data.slice.len) {
-                    return error.ArrayTooSmall;
-                }
-                mem.copy(u8, &array, column_data.slice);
-                std.debug.warn("slice: {x}\n", .{array});
+                mem.copy(u8, &array, column_data.slice[0..array.len]);
             },
             .List, .Set => {
                 unreachable;
             },
             else => {
-                unreachable;
-                // std.debug.panic("CQL type " ++ @tagName(column_spec.option.id) ++ " is not implemented");
+                std.debug.panic("CQL type {} can't be read into the type {}", .{ std.meta.tagName(column_spec.option.id), @typeName(Type) });
             },
         }
 
@@ -150,7 +174,7 @@ test "iterator scan" {
     const Row = struct {
         id: [16]u8,
         age: u8,
-        name: [64]u8,
+        name: []const u8,
     };
     var row: Row = undefined;
 
