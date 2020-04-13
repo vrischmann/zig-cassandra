121e672df548bf7eccf68e54d8e3f9f3f4bd477b
Vincent Rischmann <vincent@rischmann.fr>
2020-04-13 17:29:46 +0200

    primitive: test writeInt

--- src/primitive/writer.zig
+++ src/primitive/writer.zig
@@ -20,13 +20,13 @@ pub const PrimitiveWriter = struct {
 
     pub fn reset(self: *Self, wbuf: []u8) void {
         self.wbuf = wbuf;
-        // self.source = io.StreamSource{ .buffer = io.fixedBufferStream(wbuf) };
-        // self.out_stream = self.source.outStream();
+        self.source = io.StreamSource{ .buffer = io.fixedBufferStream(wbuf) };
+        self.out_stream = self.source.outStream();
     }
 
     /// Write either a short, a int or a long to the stream.
     pub fn writeInt(self: *Self, comptime T: type, value: T) !void {
-        // return self.out_stream.writeIntBig(T, value);
+        return self.out_stream.writeIntBig(T, value);
     }
 };
 
@@ -37,15 +37,15 @@ test "primitive writer: write int" {
 
     // read all int types
 
-    try pw.writeInt(i32, @as(i32, 2101504));
+    try pw.writeInt(i32, 2101504);
     testing.expectEqualSlices(u8, "\x00\x20\x11\x00", buf[0..4]);
 
-    // pr.reset("\x00\x00\x40\x00\x00\x20\x11\x00");
-    // testing.expectEqual(@as(i64, 70368746279168), try pr.readInt(i64));
+    try pw.writeInt(i64, 70368746279168);
+    testing.expectEqualSlices(u8, "\x00\x00\x40\x00\x00\x20\x11\x00", buf[4..12]);
 
-    // pr.reset("\x11\x00");
-    // testing.expectEqual(@as(u16, 4352), try pr.readInt(u16));
+    try pw.writeInt(i16, 4352);
+    testing.expectEqualSlices(u8, "\x11\x00", buf[12..14]);
 
-    // pr.reset("\xff");
-    // testing.expectEqual(@as(u8, 0xFF), try pr.readByte());
+    try pw.writeInt(u8, 0xFF);
+    testing.expectEqualSlices(u8, "\xff", buf[14..15]);
 }
