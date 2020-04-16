eb5510c18777e8cb2bed4e491c7aa58ddedf35a8
Vincent Rischmann <vincent@rischmann.fr>
2020-04-17 00:52:34 +0200

    frames: implement Register.write

--- src/frames/register.zig
+++ src/frames/register.zig
@@ -13,6 +13,10 @@ const testing = @import("../testing.zig");
 const RegisterFrame = struct {
     event_types: []const []const u8,
 
+    pub fn write(self: @This(), pw: *PrimitiveWriter) !void {
+        return pw.writeStringList(self.event_types);
+    }
+
     pub fn read(allocator: *mem.Allocator, pr: *PrimitiveReader) !RegisterFrame {
         return RegisterFrame{
             .event_types = try pr.readStringList(),
@@ -24,10 +28,12 @@ test "register frame" {
     var arena = testing.arenaAllocator();
     defer arena.deinit();
 
-    const data = "\x04\x00\x00\xc0\x0b\x00\x00\x00\x31\x00\x03\x00\x0f\x54\x4f\x50\x4f\x4c\x4f\x47\x59\x5f\x43\x48\x41\x4e\x47\x45\x00\x0d\x53\x54\x41\x54\x55\x53\x5f\x43\x48\x41\x4e\x47\x45\x00\x0d\x53\x43\x48\x45\x4d\x41\x5f\x43\x48\x41\x4e\x47\x45";
-    const raw_frame = try testing.readRawFrame(&arena.allocator, data);
+    // read
 
-    checkHeader(Opcode.Register, data.len, raw_frame.header);
+    const exp = "\x04\x00\x00\xc0\x0b\x00\x00\x00\x31\x00\x03\x00\x0f\x54\x4f\x50\x4f\x4c\x4f\x47\x59\x5f\x43\x48\x41\x4e\x47\x45\x00\x0d\x53\x54\x41\x54\x55\x53\x5f\x43\x48\x41\x4e\x47\x45\x00\x0d\x53\x43\x48\x45\x4d\x41\x5f\x43\x48\x41\x4e\x47\x45";
+    const raw_frame = try testing.readRawFrame(&arena.allocator, exp);
+
+    checkHeader(Opcode.Register, exp.len, raw_frame.header);
 
     var pr = PrimitiveReader.init(&arena.allocator);
     pr.reset(raw_frame.body);
@@ -38,4 +44,8 @@ test "register frame" {
     testing.expectEqualString("TOPOLOGY_CHANGE", frame.event_types[0]);
     testing.expectEqualString("STATUS_CHANGE", frame.event_types[1]);
     testing.expectEqualString("SCHEMA_CHANGE", frame.event_types[2]);
+
+    // write
+
+    testing.expectSameRawFrame(frame, raw_frame.header, exp);
 }
