4ed3226aa6ae7f86b60d68cf6563cb3aaaac8e19
Vincent Rischmann <vincent@rischmann.fr>
2020-04-18 01:28:00 +0200

    cli: create a client

--- src/cli.zig
+++ src/cli.zig
@@ -1,7 +1,12 @@
 const std = @import("std");
+const net = std.net;
 
 const cql = @import("lib.zig");
 
 pub fn main() anyerror!void {
-    std.debug.warn("prout", .{});
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const allocator = &arena.allocator;
+
+    var client = try cql.Client.init(allocator, "localhost", 9042);
 }
