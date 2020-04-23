1f287b5f324f62f1c28dbf5ea0dc46545af37f28
Vincent Rischmann <vincent@rischmann.fr>
2020-04-23 22:21:00 +0200

    cli: test

--- src/cli.zig
+++ src/cli.zig
@@ -17,22 +17,22 @@ pub fn main() anyerror!void {
     defer result_arena.deinit();
     const result_allocator = &result_arena.allocator;
 
-    // var result = try client.cquery(result_allocator, "SELECT * FROM foobar.user WHERE age = ? allow filtering", .{
-    //     .age = 120,
-    // });
-    var result = try client.cquery(result_allocator, "SELECT ids, age FROM foobar.age_to_ids WHERE age = ?", .{
-        @as(u32, 120),
+    var result = try client.cquery(result_allocator, "SELECT ids, age, name FROM foobar.age_to_ids WHERE age = ?", .{
+        .age = @as(u32, 120),
     });
+    // var result = try client.cquery(result_allocator, "SELECT ids, age FROM foobar.age_to_ids WHERE age = ?", .{
+    //     @as(u32, 120),
+    // });
     var iter = result.Iter;
 
     const Row = struct {
         ids: []u8,
         age: u32,
+        name: []const u8,
     };
     var row: Row = undefined;
 
     while (try iter.scan(&row)) {
-        std.debug.warn("age: {} id: {x}\n", .{ row.age, row.ids });
-        // std.debug.warn("age: {}\n", .{row.age});
+        std.debug.warn("age: {} id: {x} name: {} {x}\n", .{ row.age, row.ids, row.name, row.name });
     }
 }
