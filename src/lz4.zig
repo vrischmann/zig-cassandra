da2343d5c3d8c5809b54be0d715ac5bac64d9fe6
Vincent Rischmann <vincent@rischmann.fr>
2020-04-26 19:18:22 +0200

    lz4: rename functions

--- src/lz4.zig
+++ src/lz4.zig
@@ -4,7 +4,7 @@ const mem = std.mem;
 const c = @import("c.zig");
 const testing = @import("testing.zig");
 
-pub fn lz4Compress(allocator: *mem.Allocator, data: []const u8) ![]const u8 {
+pub fn compress(allocator: *mem.Allocator, data: []const u8) ![]const u8 {
     const max_dst_size = c.LZ4_compressBound(@intCast(c_int, data.len));
 
     const buf = try allocator.alloc(u8, @intCast(usize, max_dst_size));
@@ -23,7 +23,7 @@ pub fn lz4Compress(allocator: *mem.Allocator, data: []const u8) ![]const u8 {
     return buf[0..@intCast(usize, compressed_data_size)];
 }
 
-pub fn lz4Decompress(allocator: *mem.Allocator, data: []const u8, max_decompressed_size: usize) ![]const u8 {
+pub fn decompress(allocator: *mem.Allocator, data: []const u8, max_decompressed_size: usize) ![]const u8 {
     const buf = try allocator.alloc(u8, max_decompressed_size);
     errdefer allocator.free(buf);
 
@@ -41,12 +41,12 @@ pub fn lz4Decompress(allocator: *mem.Allocator, data: []const u8, max_decompress
 }
 
 test "lz4: compress and decompress" {
-    const exp = "Dolorem in eos repellat facilis voluptatum sed. Autem ipsum quaerat voluptas ut cum impedit. Ut sapiente dolor eos sit. Dolorum nihil nobis voluptas est et sunt voluptatem. Veniam labore quae explicabo.";
+    const exp = "Dolorem in eos repellat facilis voluptatum sed. Autem ipsum quaerat voluptas ut cum impedit. Ut sapiente dolor eos sit. Dolorum nihil nobis voluptas est et sunt voluptatem. Veniam labore quae explicabo." ** 10;
 
-    const compressed = try lz4Compress(testing.allocator, exp);
+    const compressed = try compress(testing.allocator, exp);
     defer testing.allocator.free(compressed);
 
-    const decompressed = try lz4Decompress(testing.allocator, compressed, exp.len);
+    const decompressed = try decompress(testing.allocator, compressed, exp.len);
     defer testing.allocator.free(decompressed);
 
     testing.expectEqualString(exp, decompressed);
