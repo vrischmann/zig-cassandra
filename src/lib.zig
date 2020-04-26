1b459314330a48cea35a96e6edf2f154d47efb86
Vincent Rischmann <vincent@rischmann.fr>
2020-04-26 22:29:15 +0200

    fix imports

--- src/lib.zig
+++ src/lib.zig
@@ -4,14 +4,14 @@ const meta = std.meta;
 const net = std.net;
 const ArrayList = std.ArrayList;
 
-pub const CompressionAlgorithm = @import("conn.zig").CompressionAlgorithm;
-pub const Client = @import("conn.zig").Client;
+pub const CompressionAlgorithm = @import("client.zig").CompressionAlgorithm;
+pub const Client = @import("client.zig").Client;
 pub const Iterator = @import("iterator.zig").Iterator;
 
 const testing = @import("testing.zig");
 
 test "" {
-    _ = @import("conn.zig");
+    _ = @import("client.zig");
     _ = @import("frame.zig");
     _ = @import("iterator.zig");
 }
