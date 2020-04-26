e62771f084b32a78c3b9b47435a66124d5ace9dd
Vincent Rischmann <vincent@rischmann.fr>
2020-04-26 19:31:56 +0200

    lz4: fix to work with the framing the cassandra uses

--- src/lz4.zig
+++ src/lz4.zig
@@ -4,15 +4,22 @@ const mem = std.mem;
 const c = @import("c.zig");
 const testing = @import("testing.zig");
 
+const length_size = 4;
+
 pub fn compress(allocator: *mem.Allocator, data: []const u8) ![]const u8 {
     const max_dst_size = c.LZ4_compressBound(@intCast(c_int, data.len));
 
-    const buf = try allocator.alloc(u8, @intCast(usize, max_dst_size));
+    const buf = try allocator.alloc(u8, @intCast(usize, max_dst_size) + length_size);
     errdefer allocator.free(buf);
+
+    // Encode the uncompressed length before the compressed byted.
+    mem.writeIntBig(u32, buf[0..length_size], @intCast(u32, data.len));
+
+    var compressed_bytes = buf[length_size..buf.len];
 
     const compressed_data_size = c.LZ4_compress_default(
         @ptrCast([*c]const u8, data),
-        @ptrCast([*c]u8, buf),
+        @ptrCast([*c]u8, compressed_bytes),
         @intCast(c_int, data.len),
         max_dst_size,
     );
@@ -20,17 +27,23 @@ pub fn compress(allocator: *mem.Allocator, data: []const u8) ![]const u8 {
         return error.CompressionFailed;
     }
 
-    return buf[0..@intCast(usize, compressed_data_size)];
+    return buf[0 .. @intCast(usize, compressed_data_size) + length_size];
 }
 
-pub fn decompress(allocator: *mem.Allocator, data: []const u8, max_decompressed_size: usize) ![]const u8 {
+pub fn decompress(allocator: *mem.Allocator, data: []const u8) ![]const u8 {
+    // The uncompressed length is encoded as 4 bytes before the compressed byted.
+    var max_decompressed_size = mem.readIntBig(u32, data[0..length_size]);
+
+    // The rest are the compressed bytes.
+    const compressed_data = data[length_size..];
+
     const buf = try allocator.alloc(u8, max_decompressed_size);
     errdefer allocator.free(buf);
 
     const decompressed_size = c.LZ4_decompress_safe(
-        @ptrCast([*c]const u8, data),
+        @ptrCast([*c]const u8, compressed_data),
         @ptrCast([*c]u8, buf),
-        @intCast(c_int, data.len),
+        @intCast(c_int, compressed_data.len),
         @intCast(c_int, buf.len),
     );
     if (decompressed_size <= 0) {
@@ -46,7 +59,7 @@ test "lz4: compress and decompress" {
     const compressed = try compress(testing.allocator, exp);
     defer testing.allocator.free(compressed);
 
-    const decompressed = try decompress(testing.allocator, compressed, exp.len);
+    const decompressed = try decompress(testing.allocator, compressed);
     defer testing.allocator.free(decompressed);
 
     testing.expectEqualString(exp, decompressed);
