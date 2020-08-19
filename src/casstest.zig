625c90d600dcbd7213d5e9690dc24aea2e68a8d8
Vincent Rischmann <vincent@rischmann.fr>
2020-08-19 22:59:26 +0200

    casstest: take the protocol version as parameter

--- src/casstest.zig
+++ src/casstest.zig
@@ -56,10 +56,11 @@ pub const Row = struct {
     };
 };
 
-pub fn initTestClient(allocator: *mem.Allocator) !*Client {
+pub fn initTestClient(allocator: *mem.Allocator, protocol_version: ProtocolVersion) !*Client {
     var address = std.net.Address.initIp4([_]u8{ 127, 0, 0, 1 }, 9042);
 
     var init_options = InitOptions{};
+    init_options.protocol_version = protocol_version;
     init_options.compression = CompressionAlgorithm.LZ4;
     init_options.username = "cassandra";
     init_options.password = "cassandra";
