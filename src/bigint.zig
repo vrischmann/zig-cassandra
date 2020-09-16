cd69be0332d46cc89a9027dc1dbcd48606642408
Vincent Rischmann <vincent@rischmann.fr>
2020-09-16 22:17:34 +0200

    bigint: fix toBytes

--- src/bigint.zig
+++ src/bigint.zig
@@ -131,23 +131,80 @@ pub fn toBytes(allocator: *mem.Allocator, n: big.int.Const) ![]const u8 {
         return b;
     }
 
-    var buf = try allocator.alloc(u8, n.limbs.len * 8);
-    errdefer allocator.free(buf);
+    // Handle positive numbers
+    if (n.positive) {
+        var buf = try allocator.alloc(u8, n.limbs.len * 8);
+        errdefer allocator.free(buf);
 
-    var pos = try rawBytes(buf, n);
+        var pos = try rawBytes(buf, n);
 
-    // This is for §6.23 https://github.com/apache/cassandra/blob/trunk/doc/native_protocol_v5.spec#L1037-L1040
-
-    if (n.positive and (buf[0] & 0x80) > 0) {
-        buf[pos - 1] = 0;
-        return buf[pos - 1 ..];
+        // This is for §6.23 https://github.com/apache/cassandra/blob/trunk/doc/native_protocol_v5.spec#L1037-L1040
+        if ((buf[0] & 0x80) > 0) {
+            buf[pos - 1] = 0;
+            return buf[pos - 1 ..];
+        }
+        return buf[pos..];
     }
 
-    // TODO(vincent): implement the case where it's negative
-    unreachable;
+    // Handle negative numbers
+
+    const shift = (n.bitCountTwosComp() / 8 + 1) * 8;
+
+    // Capacity is:
+    // * one for the 1 big.int
+    // * how many full limbs are in the data slice
+    // * one more to store the remaining bytes
+    const capacity = (shift / limb_bytes) + 1;
+
+    // Allocate a temporary big.int to hold the shifted value.
+    var tmp = try big.int.Managed.initCapacity(allocator, capacity);
+    defer tmp.deinit();
+    var tmp_mutable = tmp.toMutable();
+
+    // 1 << shift
+    tmp_mutable.shiftLeft(big_one, shift);
+
+    // n = n + (1 << shift)
+    var n_tmp = try n.toManaged(allocator);
+    defer n_tmp.deinit();
+
+    try n_tmp.add(n_tmp.toConst(), tmp_mutable.toConst());
+
+    var buf = try allocator.alloc(u8, (n_tmp.len() + 1) * limb_bytes);
+    var pos = try rawBytes(buf, n_tmp.toConst());
+
+    return buf[pos..];
 }
 
-test "bigint: toBytes" {}
+test "bigint: toBytes" {
+    const testCase = struct {
+        n: []const u8,
+        exp_data: []const u8,
+    };
+
+    const testCases = [_]testCase{
+        testCase{
+            .n = "3405245950896869895938539859386968968953285938539111111111111111111111111111111111111111122222222222222222222222222222222",
+            .exp_data = "\x01\x51\x97\x29\x61\xfa\xec\x86\x0d\x18\x06\x93\xd0\x53\xba\xb9\x02\xbd\xde\xe9\xfa\xab\x78\xcf\x52\x2e\x43\x1c\x68\xea\x9e\xb8\xeb\x35\x6e\x96\x31\xfc\x4b\x10\xfc\x9d\x0a\x2b\x4c\x1a\x9c\x8e\x38\xe3\x8e",
+        },
+        testCase{
+            .n = "-3405245950896869895938539859386968968953285938539111111111111111111111111111111111111111122222222222222222222222222222222",
+            .exp_data = "\xfe\xae\x68\xd6\x9e\x05\x13\x79\xf2\xe7\xf9\x6c\x2f\xac\x45\x46\xfd\x42\x21\x16\x05\x54\x87\x30\xad\xd1\xbc\xe3\x97\x15\x61\x47\x14\xca\x91\x69\xce\x03\xb4\xef\x03\x62\xf5\xd4\xb3\xe5\x63\x71\xc7\x1c\x72",
+        },
+    };
+
+    inline for (testCases) |tc| {
+        var n = try big.int.Managed.init(testing.allocator);
+        defer n.deinit();
+
+        try n.setString(10, tc.n);
+
+        const data = try toBytes(testing.allocator, n.toConst());
+        defer testing.allocator.free(data);
+
+        testing.expectEqualSlices(u8, tc.exp_data, data);
+    }
+}
 
 test "bigint: fromBytes" {
     const testCase = struct {
