d3f13c4329eafd8a94ae47d8c9ebec80704a13b2
Vincent Rischmann <vincent@rischmann.fr>
2024-05-29 00:32:02 +0200

    bigint: refactor fromBytes

--- src/bigint.zig
+++ src/bigint.zig
@@ -67,18 +67,25 @@ const big_one = big.int.Const{
 ///
 /// Based on https://github.com/gocql/gocql/blob/5378c8f664e946e421b16a490513675b8419bdc7/marshal.go#L1096-L1108
 pub fn fromBytes(allocator: mem.Allocator, data: []const u8) !big.int.Managed {
+    var arena = std.heap.ArenaAllocator.init(allocator);
+    defer arena.deinit();
+
     // Get the raw big.int without worrying about the sign.
-    var n = try fromRawBytes(allocator, data);
+    const n = try fromRawBytes(arena.allocator(), data);
 
     // Per the spec, a MSB of 1 in the data means it's negative.
 
+    //
+    // Positive number
+    //
+
     if (data.len <= 0 or (data[0] & 0x80 == 0)) {
-        return n;
+        return try n.cloneWithDifferentAllocator(allocator);
     }
 
-    // NOTE(vincent): we have to make a new big.int.Managed to return
-    // because we can't use n to make the subtraction.
-    defer n.deinit();
+    //
+    // Negative number
+    //
 
     const shift = data.len * 8;
     // Capacity is:
@@ -87,17 +94,19 @@ pub fn fromBytes(allocator: mem.Allocator, data: []const u8) !big.int.Managed {
     // * one more to store the remaining bytes
     const capacity = big_one.limbs.len + (data.len / limb_bytes) + 1;
 
-    // Allocate a temporary big.int to hold the shifted value.
-    var tmp = try big.int.Managed.initCapacity(allocator, capacity);
-    defer tmp.deinit();
-    var tmp_mutable = tmp.toMutable();
-
     // 1 << shift
-    tmp_mutable.shiftLeft(big_one, shift);
+    const one_shifted = blk: {
+        var one_shifted = try big.int.Managed.initCapacity(arena.allocator(), capacity);
+        var one_shifted_mutable = one_shifted.toMutable();
+
+        one_shifted_mutable.shiftLeft(big_one, shift);
+
+        break :blk one_shifted_mutable.toManaged(arena.allocator());
+    };
 
     // res = n - (1 << shift)
     var res = try big.int.Managed.init(allocator);
-    try res.sub(&n, &tmp);
+    try res.sub(&n, &one_shifted);
 
     return res;
 }
@@ -147,8 +156,10 @@ pub fn toBytes(allocator: mem.Allocator, n: big.int.Const) ![]const u8 {
         return b;
     }
 
-    var buf = std.ArrayList(u8).init(allocator);
-    defer buf.deinit();
+    var arena = std.heap.ArenaAllocator.init(allocator);
+    defer arena.deinit();
+
+    var buf = std.ArrayList(u8).init(arena.allocator());
 
     //
     // Handle positive numbers
@@ -176,9 +187,6 @@ pub fn toBytes(allocator: mem.Allocator, n: big.int.Const) ![]const u8 {
     //
     // Handle negative numbers
     //
-
-    var arena = std.heap.ArenaAllocator.init(allocator);
-    defer arena.deinit();
 
     const shift = (n.bitCountTwosComp() / 8 + 1) * 8;
 
@@ -283,30 +291,30 @@ test "bigint: toBytes" {
     }
 }
 //
-// test "bigint: fromBytes" {
-//     const testCase = struct {
-//         data: []const u8,
-//         exp: []const u8,
-//     };
-//
-//     const testCases = [_]testCase{
-//         testCase{
-//             .data = "\x01\x51\x97\x29\x61\xfa\xec\x86\x0d\x18\x06\x93\xd0\x53\xba\xb9\x02\xbd\xde\xe9\xfa\xab\x78\xcf\x52\x2e\x43\x1c\x68\xea\x9e\xb8\xeb\x35\x6e\x96\x31\xfc\x4b\x10\xfc\x9d\x0a\x2b\x4c\x1a\x9c\x8e\x38\xe3\x8e",
-//             .exp = "3405245950896869895938539859386968968953285938539111111111111111111111111111111111111111122222222222222222222222222222222",
-//         },
-//         testCase{
-//             .data = "\xfe\xae\x68\xd6\x9e\x05\x13\x79\xf2\xe7\xf9\x6c\x2f\xac\x45\x46\xfd\x42\x21\x16\x05\x54\x87\x30\xad\xd1\xbc\xe3\x97\x15\x61\x47\x14\xca\x91\x69\xce\x03\xb4\xef\x03\x62\xf5\xd4\xb3\xe5\x63\x71\xc7\x1c\x72",
-//             .exp = "-3405245950896869895938539859386968968953285938539111111111111111111111111111111111111111122222222222222222222222222222222",
-//         },
-//     };
-//
-//     inline for (testCases) |tc| {
-//         var n = try fromBytes(testing.allocator, tc.data);
-//         defer n.deinit();
-//
-//         var buf: [1024]u8 = undefined;
-//         const formatted_n = try std.fmt.bufPrint(&buf, "{}", .{n});
-//
-//         try testing.expectEqualStrings(tc.exp, formatted_n);
-//     }
-// }
+test "bigint: fromBytes" {
+    const testCase = struct {
+        data: []const u8,
+        exp: []const u8,
+    };
+
+    const testCases = [_]testCase{
+        testCase{
+            .data = "\x01\x51\x97\x29\x61\xfa\xec\x86\x0d\x18\x06\x93\xd0\x53\xba\xb9\x02\xbd\xde\xe9\xfa\xab\x78\xcf\x52\x2e\x43\x1c\x68\xea\x9e\xb8\xeb\x35\x6e\x96\x31\xfc\x4b\x10\xfc\x9d\x0a\x2b\x4c\x1a\x9c\x8e\x38\xe3\x8e",
+            .exp = "3405245950896869895938539859386968968953285938539111111111111111111111111111111111111111122222222222222222222222222222222",
+        },
+        testCase{
+            .data = "\xfe\xae\x68\xd6\x9e\x05\x13\x79\xf2\xe7\xf9\x6c\x2f\xac\x45\x46\xfd\x42\x21\x16\x05\x54\x87\x30\xad\xd1\xbc\xe3\x97\x15\x61\x47\x14\xca\x91\x69\xce\x03\xb4\xef\x03\x62\xf5\xd4\xb3\xe5\x63\x71\xc7\x1c\x72",
+            .exp = "-3405245950896869895938539859386968968953285938539111111111111111111111111111111111111111122222222222222222222222222222222",
+        },
+    };
+
+    inline for (testCases) |tc| {
+        var n = try fromBytes(testing.allocator, tc.data);
+        defer n.deinit();
+
+        var buf: [1024]u8 = undefined;
+        const formatted_n = try std.fmt.bufPrint(&buf, "{}", .{n});
+
+        try testing.expectEqualStrings(tc.exp, formatted_n);
+    }
+}
