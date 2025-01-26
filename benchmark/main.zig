5810eb37824c1963431e3fd943fbd9b684313fbf
Vincent Rischmann <vincent@rischmann.fr>
2025-01-26 15:31:33 +0100

    wip

--- benchmark/main.zig
+++ benchmark/main.zig
@@ -1,3 +1,14 @@
 const std = @import("std");
+const debug = std.debug;
+const heap = std.heap;
+const net = std.net;
 
-pub fn main() anyerror!void {}
+pub fn main() anyerror!void {
+    var gpa: heap.GeneralPurposeAllocator(.{}) = .init;
+    defer debug.assert(gpa.deinit() == .ok);
+
+    const allocator = gpa.allocator();
+
+    // const address = try net.Ip4Address.init([4]u8{ 127, 0, 0, 1 }, 9042);
+    // const socket = try net.tcpConnectToAddress(address);
+}
